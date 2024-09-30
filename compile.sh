02904f93d0a67e1fed3e5bb43bf819190359e53f
Charli <chly-huc@student.s19.be>
2024-09-30 22:23:45 +0200

    pushed

--- compile.sh
+++ compile.sh
@@ -4,6 +4,6 @@ nasm -f elf64 boot.s
 nasm -f elf64 header.s 
 
 ~/opt/x86_64-pc-elf/bin/ld --nmagic --output=kernel.bin --script=linker.ld header.o boot.o
-cp Kernel.bin isofiles/boot/
+cp kernel.bin isofiles/boot/
 
 echo Done
